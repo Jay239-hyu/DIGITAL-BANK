43c62abe4738dcf8d47a28b2ed5468bb6594200f
Jay239-hyu <jaydvaland2006@gmail.com>
2025-12-30 22:53:32 +0530

    improve workflow , fix bugs

--- BankData.py
+++ BankData.py
@@ -1,4 +1,5 @@
-import os , json
+import os , json , getpass 
+from utils import hash_pin
 from Account import Account
 
 ACCOUNT_FILE = "accounts.json"
@@ -30,24 +31,38 @@ class BankData:
     
     @staticmethod
     def load_account_by_acc_no(account_number):
-        if account_number in accounts:
-            data = accounts[account_number]
-            acc = Account(account_number, data["name"], data["pin"], data["balance"]) #Here acc is a object of Account class.
-            acc.load_history(data.get("history" , []))
-            return acc
-        else: 
-            print("Sorry, can't find your account in our database please ensure that your bank account was created in our Bank. Thank You! ")
-      
+        if account_number not in accounts:
+            return ("NO_ACCOUNT" , None)
+
+        data = accounts[account_number]
+        acc = Account(account_number, data["name"], data["pin"], data["balance"]) #Here acc is a object of Account class.
+        acc.load_history(data.get("history" , []))
+        return ("SUCCESS",acc)
     
     @staticmethod
     def load_account(account_number, hashed_input):
-        if account_number in accounts and accounts[account_number]["pin"] == hashed_input:
-            data = accounts[account_number]
-            acc = Account(account_number, data["name"], data["pin"], data["balance"])
-            acc.load_history(data.get("history" , []))
-            return acc
-        else:
-            print("Sorry, can't find This account in our database please ensure that this account is exitsted in out bank or not, Thank You!")
+        if account_number not in accounts:
+            return ("NO_ACCOUNT" , None)
+        
+        if accounts[account_number]["pin"] != hashed_input:
+            return ("WRONG_PIN" , None)
+        
+        data = accounts[account_number]
+        acc = Account(account_number, data["name"], data["pin"], data["balance"])
+        acc.load_history(data.get("history" , []))
+        return ("SUCCESS" , acc)
+     
+    @staticmethod
+    def create_account(name , pin):
+            if len(pin) != 4 or not pin.isdigit():
+                return("INVALID PIN",None)
+        
+            hashed_pin = hash_pin(pin)
+            account_number = str(len(accounts) + 1001)
+            new_account = Account(account_number, name, hashed_pin) #Here New account is a object of Bankaccount class.
+            BankData.save_account(account_number, new_account)
+            return ("SUCCESS" , account_number)
+
       
         
 
